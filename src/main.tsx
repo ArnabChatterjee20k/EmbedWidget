50add987fb4d5641903391cb180ac451125b2e72
Arnab20k-Peakflo <arnab.chatterjee@peakflo.co>
2024-10-25 00:45:17 +0530

    test

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+
+const palette = document.querySelectorAll(".pickpalette-component");
+palette.forEach((div) =>
+  createRoot(div).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
+);
